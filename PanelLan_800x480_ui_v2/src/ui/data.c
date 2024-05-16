39074fd2db59fa8dec85f92876883b4e8a73ac56
garaase <feysal.ali1@yahoo.com>
2024-05-16 18:44:55 +0100

    Updated PanelLan_800x480_ui_v2

---
+++ PanelLan_800x480_ui_v2/src/ui/data.c
@@ -0,0 +1,4 @@
+//extern lv_coord_t __chart1_series_array[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+#include "lvgl.h"
+lv_coord_t * data_Chart1_series_array;
+lv_coord_t * data_Chart2_series_array;
---
+++ PanelLan_800x480_ui_v2/src/ui/data.h
@@ -0,0 +1,3 @@
+#include "lvgl.h"
+extern lv_coord_t * data_Chart1_series_array;
+extern lv_coord_t * data_Chart2_series_array;
