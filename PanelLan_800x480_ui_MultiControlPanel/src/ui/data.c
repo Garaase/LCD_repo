c0875a7387da1a021e4607abe0ea7fbb16b07784
garaase <feysal.ali1@yahoo.com>
2024-06-05 21:00:35 +0100

    Added PanelLan_800x480_ui_MultiControlPanel

---
+++ PanelLan_800x480_ui_MultiControlPanel/src/ui/data.c
@@ -0,0 +1,14 @@
+//extern lv_coord_t __chart1_series_array[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+#include "lvgl.h"
+//lv_coord_t * data_Chart1_series_array;
+//lv_coord_t * data_Chart2_series_array;
+bool BtnRelay1Toggle = false;
+bool BtnRelay2Toggle = false;
+bool BtnRelay3Toggle = false;
+bool BtnRelay4Toggle = false;
+bool BtnRelaysToggled = false;
+char selectWifiNode = 1;
+bool switch1Status;
+char _SWITCHSTATUS;
+
+enum SWITCHSTATUS; 
